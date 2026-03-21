05a368aad3915bd5a9264436660aafa1de82dedc
solreven <blomlie@proton.me>
2026-03-21 11:33:17 +0100

    v.0.02 - Fixed the layout for the header

--- app/components/PageLayout.tsx
+++ app/components/PageLayout.tsx
@@ -15,26 +15,26 @@ export default function PageLayout({ children }: PageLayoutProps) {
     <main
       className={`h-screen flex flex-col items-center justify-around gap-1`}
     >
-      <nav className={`flex w-screen justify-evenly`}>
-        <h1>Brian Blitz</h1>
-        <Link href="../">Hjem</Link>
-        <Link href="../projects">Projekter</Link>
-        <Link href="../about">Om meg</Link>
-      </nav>
       <ClerkProvider>
-        <header className="flex justify-end items-center p-4 gap-4 h-16">
-          <Show when="signed-out">
-            <SignInButton />
-            <SignUpButton>
-              <button className="bg-[#6c47ff] text-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
-                Sign Up
-              </button>
-            </SignUpButton>
-          </Show>
-          <Show when="signed-in">
-            <UserButton />
-          </Show>
-        </header>
+        <nav className={`flex w-screen justify-evenly items-center`}>
+          <h1>Brian Blitz</h1>
+          <Link href="../">Hjem</Link>
+          <Link href="../projects">Projekter</Link>
+          <Link href="../about">Om meg</Link>
+          <div className="flex justify-end items-center p-4 gap-4 h-16">
+            <Show when="signed-out">
+              <SignInButton />
+              <SignUpButton>
+                <button className="bg-[#6c47ff] text-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
+                  Sign Up
+                </button>
+              </SignUpButton>
+            </Show>
+            <Show when="signed-in">
+              <UserButton />
+            </Show>
+          </div>
+        </nav>
         {children}
       </ClerkProvider>
     </main>
